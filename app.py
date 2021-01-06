4751404e7a38967d63553d7778864321b36b3e83
Dinesh <dineshchander1995@gmail.com>
2021-01-06 17:36:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response, abort
 from datetime import datetime, timedelta
 import requests
 import pdb
@@ -9,14 +9,33 @@ import os
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+auth_token = ''
 
+def get_auth_token(force=False):
+	global auth_token
+	if auth_token == '' or force:
+		auth_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v1/auth"
+		auth_headers = {'Content-type': 'application/json'}
+		auth_data = {"username":os.environ.get('db_username'),"password":os.environ.get('db_password')}
+		response = requests.post(auth_url, headers=auth_headers, json=auth_data)
+		auth_token = response.json()['authToken']
+	return auth_token
 
-def get_auth_token():
-	auth_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v1/auth"
-	auth_headers = {'Content-type': 'application/json'}
-	auth_data = {"username":os.environ.get('db_username'),"password":os.environ.get('db_password')}
-	response = requests.post(auth_url, headers=auth_headers, json=auth_data)
-	return response.json()['authToken']
+def cassandra_request(type, url, data={}, params=""):
+	base_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com"
+	url = base_url + url
+	try:
+		headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
+		response = requests.request(type, url, headers=headers, json=data, params=params)
+		if response.status_code == 401:
+			headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token(True)}
+			response = requests.request(type, url, headers=headers, json=data, params=params)
+		if type != 'DELETE':
+				response = response.json()
+		return response
+	except Exception as e:
+		print("DB Error - " + str(e))
+		abort(Response(status=500, response=json.dumps({"db_connection_error":"Error in connecting with Datastax Astra DB. Try again after sometime"}), mimetype='application/json'))
 
 # Create Patient (User Singup)
 # {
@@ -35,8 +54,7 @@ def get_auth_token():
 # 			"value":"Sometimes"
 # 		},{	"key":"Are you spiritual",
 # 			"value":"Yes"
-#		},{
-# 			"key":"Is your financial condition bothering you?",
+#		},{ "key":"Is your financial condition bothering you?",
 # 			"value":"Yes it does"	
 # 		}
 # 	]
@@ -46,12 +64,11 @@ def create_patient():
 	request.json['id'] = str(uuid.uuid1())
 	request.json['type'] = "patient"
 	request.json['password'] = bcrypt.generate_password_hash(request.json['password']).decode("utf-8")
-	patient_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users"
+	patient_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users"
+	response = cassandra_request('POST', patient_url, request.json)
+	patient_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + response['id']
 	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.post(patient_url, headers=headers, json=request.json).json()
-	patient_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + response['id']
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(patient_url, headers=headers).json()
+	response = cassandra_request('GET', patient_url)
 	del response['data'][0]['doctor_details']
 	return response
 
@@ -61,12 +78,11 @@ def create_patient():
 # }
 @app.route('/api/users/patient/<user_id>', methods=['PUT'])
 def update_patient(user_id):
-	patient_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
+	patient_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
+	response = cassandra_request('PATCH', patient_url, request.json)
+	patient_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
 	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.patch(patient_url, headers=headers, json=request.json).json()
-	patient_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(patient_url, headers=headers).json()
+	response = cassandra_request('GET', patient_url)
 	del response['data'][0]['doctor_details']
 	return response
 
@@ -83,8 +99,7 @@ def update_patient(user_id):
 # 	"doctor_details": [{
 # 			"key":"Specialization",
 # 			"value":"Phyciatrist"
-# 		},{
-# 			"key":"Experience",
+# 		},{ "key":"Experience",
 # 			"value":"10 years"	
 # 		}
 # 	]
@@ -94,12 +109,10 @@ def create_doctor():
 	request.json['id'] = str(uuid.uuid1())
 	request.json['type'] = "doctor"
 	request.json['password'] = bcrypt.generate_password_hash(request.json['password']).decode("utf-8")
-	doctor_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users"
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.post(doctor_url, headers=headers, json=request.json).json()
-	doctor_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + response['id']
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(doctor_url, headers=headers).json()
+	doctor_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users"
+	response = cassandra_request('POST', doctor_url, request.json)
+	doctor_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + response['id']
+	response = cassandra_request('GET', doctor_url)
 	del response['data'][0]['patient_details']
 	return response
 
@@ -109,21 +122,18 @@ def create_doctor():
 # }
 @app.route('/api/users/doctor/<user_id>', methods=['PUT'])
 def update_doctor(user_id):
-	doctor_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.patch(doctor_url, headers=headers, json=request.json).json()
-	doctor_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(doctor_url, headers=headers).json()
+	doctor_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
+	response = cassandra_request('PATCH', doctor_url, request.json)
+	doctor_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
+	response = cassandra_request('GET', doctor_url)
 	del response['data'][0]['patient_details']
 	return response
 
 # View all users
 @app.route('/api/users')
 def users():
-	users_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v1/keyspaces/healthapp_keyspace/tables/users/rows"
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(users_url, headers=headers).json()
+	users_url = "/api/rest/v1/keyspaces/healthapp_keyspace/tables/users/rows"
+	response = cassandra_request('GET', users_url)
 	for index, data in enumerate(response['rows']):
 		if data['type'] == "patient":
 			del response['rows'][index]['doctor_details']
@@ -134,9 +144,8 @@ def users():
 # View single user - Can be patient or doctor
 @app.route('/api/user/<user_id>')
 def view_user(user_id):
-	users_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(users_url, headers=headers).json()
+	users_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + user_id
+	response = cassandra_request('GET', users_url)
 	if response['data'][0]['type'] == "patient":
 		del response['data'][0]['doctor_details']
 	else:
@@ -146,10 +155,9 @@ def view_user(user_id):
 # View All Doctors
 @app.route('/api/users/doctors', methods=['GET'])
 def doctors():
-	find_doctors_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users"
+	find_doctors_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users"
 	query_data = {'type': {'$eq': "doctor"}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(find_doctors_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', find_doctors_url, {}, {'where':json.dumps(query_data)})
 	for index, data in enumerate(response['data']):
 		del response['data'][index]['patient_details']
 	return response
@@ -157,10 +165,9 @@ def doctors():
 # View All Patients
 @app.route('/api/users/patients', methods=['GET'])
 def patients():
-	find_patients_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users"
+	find_patients_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users"
 	query_data = {'type': {'$eq': "patient"}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(find_patients_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', find_patients_url, {}, {'where':json.dumps(query_data)})
 	for index, data in enumerate(response['data']):
 		del response['data'][index]['doctor_details']
 	return response
@@ -175,15 +182,13 @@ def patients():
 def users_login():
 	user_email = request.json['email']
 	user_password = request.json['password']
-	find_user_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users"
+	find_user_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users"
 	query_data = {'email': {'$eq': user_email}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(find_user_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', find_user_url, {}, {'where':json.dumps(query_data)})
 	if response['count'] == 1:
 		if bcrypt.check_password_hash(response['data'][0]['password'],user_password):
-			user_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/users/" + response['data'][0]['id']
-			headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-			response = requests.get(user_url, headers=headers).json()
+			user_url = "/api/rest/v2/keyspaces/healthapp_keyspace/users/" + response['data'][0]['id']
+			response = cassandra_request('GET', user_url)
 			if response['data'][0]['type'] == "patient":
 				del response['data'][0]['doctor_details']
 			else:
@@ -206,12 +211,10 @@ def users_login():
 @app.route('/api/book_appointment', methods=['POST'])
 def book_appointment():
 	request.json['id'] = str(uuid.uuid1())
-	appointments_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/appointments"
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.post(appointments_url, headers=headers, json=request.json).json()
-	appointments_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/appointments/" + response['id']
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(appointments_url, headers=headers).json()
+	appointments_url = "/api/rest/v2/keyspaces/healthapp_keyspace/appointments"
+	response = cassandra_request('POST', appointments_url, request.json)
+	appointments_url = "/api/rest/v2/keyspaces/healthapp_keyspace/appointments/" + response['id']
+	response = cassandra_request('GET', appointments_url)
 	response['data'][0]['start_time'] = (datetime.fromtimestamp(response['data'][0]['start_time']['epochSecond']) - timedelta(hours=5, minutes=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
 	response['data'][0]['end_time'] = (datetime.fromtimestamp(response['data'][0]['end_time']['epochSecond']) - timedelta(hours=5, minutes=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
 	response['success'] = 'Appointment created successfully'
@@ -220,27 +223,30 @@ def book_appointment():
 # View Appointments of a doctor
 @app.route('/api/view_appointment/doctor/<user_id>', methods=['GET'])
 def view_appointment_doctor(user_id):
-	appointments_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/appointments"
+	appointments_url = "/api/rest/v2/keyspaces/healthapp_keyspace/appointments"
 	query_data = {'doctor_id': {'$eq': user_id}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(appointments_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', appointments_url, {}, {'where':json.dumps(query_data)})
+	for index, data in enumerate(response['data']):
+		response['data'][index]['start_time'] = (datetime.fromtimestamp(response['data'][0]['start_time']['epochSecond']) - timedelta(hours=5, minutes=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
+		response['data'][index]['end_time'] = (datetime.fromtimestamp(response['data'][0]['end_time']['epochSecond']) - timedelta(hours=5, minutes=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
 	return response
 
 # View Appointments of a patient
 @app.route('/api/view_appointment/patient/<user_id>', methods=['GET'])
 def view_appointment_patient(user_id):
-	appointments_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/appointments"
+	appointments_url = "/api/rest/v2/keyspaces/healthapp_keyspace/appointments"
 	query_data = {'patient_id': {'$eq': user_id}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(appointments_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', appointments_url, {}, {'where':json.dumps(query_data)})
+	for index, data in enumerate(response['data']):
+		response['data'][index]['start_time'] = (datetime.fromtimestamp(response['data'][0]['start_time']['epochSecond']) - timedelta(hours=5, minutes=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
+		response['data'][index]['end_time'] = (datetime.fromtimestamp(response['data'][0]['end_time']['epochSecond']) - timedelta(hours=5, minutes=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
 	return response
 
 # Delete Appointments
 @app.route('/api/delete_appointment/<id>', methods=['DELETE'])
 def delete_appointment(id):
-	appointments_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/appointments/"+id
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.delete(appointments_url, headers=headers)
+	appointments_url = "/api/rest/v2/keyspaces/healthapp_keyspace/appointments/"+id
+	response = cassandra_request('DELETE', appointments_url)
 	if response.status_code == 204:
 		return {"success":"Appointment deleted"}, 204
 	else:
@@ -258,18 +264,18 @@ def delete_appointment(id):
 @app.route('/api/add_medicine', methods=['POST'])
 def add_medicine():
 	request.json['id'] = str(uuid.uuid1())
-	medicines_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/medicines"
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.post(medicines_url, headers=headers, json=request.json).json()
+	medicines_url = "/api/rest/v2/keyspaces/healthapp_keyspace/medicines"
+	response = cassandra_request('POST', medicines_url, request.json)
+	medicines_url = "/api/rest/v2/keyspaces/healthapp_keyspace/medicines/" + response['id']
+	response = cassandra_request('GET', medicines_url)
 	return response
 
 # View all medicines of a patient
 @app.route('/api/medicines/patient/<user_id>', methods=['GET'])
 def view_medicines_patient(user_id):
-	medicines_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/medicines"
+	medicines_url = "/api/rest/v2/keyspaces/healthapp_keyspace/medicines"
 	query_data = {'patient_id': {'$eq': user_id}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(medicines_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', medicines_url, {}, {'where':json.dumps(query_data)})
 	return response
 
 # View medicines of a patient at specific day and time - Sample payload
@@ -277,11 +283,10 @@ def view_medicines_patient(user_id):
 @app.route('/api/medicines/patient/<user_id>/notify', methods=['GET'])
 def view_medicines_patient_notify(user_id):
 	day_time = request.args['day_time']
-	medicines_url = "https://04f17b24-94cd-447b-82ef-1b391e99778e-us-east1.apps.astra.datastax.com/api/rest/v2/keyspaces/healthapp_keyspace/medicines"
+	medicines_url = "/api/rest/v2/keyspaces/healthapp_keyspace/medicines"
 	#query_data = {'patient_id': {'$eq': user_id}, 'intake_day_time': {'$contains':day_time}}
 	query_data = {'patient_id': {'$eq': user_id}}
-	headers = {'Content-type': 'application/json','x-cassandra-token': get_auth_token()}
-	response = requests.get(medicines_url, headers=headers, params={'where':json.dumps(query_data)}).json()
+	response = cassandra_request('GET', medicines_url, {}, {'where':json.dumps(query_data)})
 	notify = {"notify" : []}
 	if response["data"]:
 		for data in response["data"]:
